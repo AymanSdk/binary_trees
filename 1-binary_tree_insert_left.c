80304281a079b46c00e8137ac0f174257ec9ba2d
AymanSdk <ayman.sdk123@oulook.com>
2023-11-28 22:06:29 +0100

    insert to left node fixed and betty also fixed

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_right - inserts a node as the left-child of another node
+ * binary_tree_insert_left - inserts a node as the left-child of another node
  * @parent: pointer to the node to insert the left-child in
  * @value:  is the value to store in the new node
  * Return: a pointer to the created node, or NULL on failure if parent is NULL
@@ -23,7 +23,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	node->left = NULL;
 	node->right = NULL;
 	if (parent->left == NULL)
-			parent->left = node;
+		parent->left = node;
 	else
 	{
 		node->left = parent->left;
