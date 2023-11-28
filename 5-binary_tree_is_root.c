d745b45f53de567b59dcb580d3eb97a0f7fd01a7
AymanSdk <ayman.sdk123@outlook.com>
2023-11-28 14:33:20 +0100

    fixing betty

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -7,13 +7,11 @@
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
-    // Check if the input node is NULL (empty)
 	if (node == NULL)
-		return (0); // If NULL, it's not a root node by definition
+	return (0);
 
-    // Check if the input node has no parent, meaning it's a root node
-    if (node->parent == NULL)
-		return (1); // Node has no parent, hence it's a root node
-    else
-		return (0); // Node has a parent, so it's not a root node
+	if (node->parent == NULL)
+		return (1);
+	else
+		return (0);
 }
