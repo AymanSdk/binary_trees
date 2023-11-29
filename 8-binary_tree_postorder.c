e0fb4b442127b5a48bd31078d08452023eed0b93
AymanSdk <ayman.sdk123@oulook.com>
2023-11-29 14:28:18 +0100

    Fixing same mistake as Right node

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -11,8 +11,8 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree && func)
 	{
-		binary_tree_inorder(tree->left, *func);
-		binary_tree_inorder(tree->right, *func);
+		binary_tree_postorder(tree->left, *func);
+		binary_tree_postorder(tree->right, *func);
 		func(tree->n);
 	}
 }
